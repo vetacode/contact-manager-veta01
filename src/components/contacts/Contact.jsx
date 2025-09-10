1c34a7c8fca2abc2250b3af231caa78ac821c1aa
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-09-10 11:22:41 +0700

    contact.jsx update

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,52 +1,52 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { deleteContact } from "../../actions/contactActions";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { deleteContact } from '../../actions/contactActions';
 
 class Contact extends Component {
   state = {
-    showContactInfo: false
+    showContactInfo: false,
   };
 
   render() {
     const { id, name, email, phone } = this.props.contact;
     return (
-      <div className="card mb-2">
-        <div className="card-header">
+      <div className='card mb-2'>
+        <div className='card-header'>
           <h4>
             {name}
             <i
-              className="fa fa-sort-down ml-2"
-              style={{ cursor: "pointer" }}
+              className='fa fa-sort-down ml-2'
+              style={{ cursor: 'pointer' }}
               onClick={() =>
                 this.setState({
-                  showContactInfo: !this.state.showContactInfo
+                  showContactInfo: !this.state.showContactInfo,
                 })
               }
             ></i>
             <i
-              className="fa fa-times"
-              style={{ cursor: "pointer", float: "right", color: "red" }}
+              className='fa fa-times'
+              style={{ cursor: 'pointer', float: 'right', color: 'red' }}
               onClick={() => this.props.deleteContact(id)}
             ></i>
             <Link to={`contact/edit/${id}`}>
               <i
-                className="fa fa-pencil"
+                className='fa fa-pencil'
                 style={{
-                  cursor: "pointer",
-                  float: "right",
-                  color: "black",
-                  marginRight: "1rem"
+                  cursor: 'pointer',
+                  float: 'right',
+                  color: 'black',
+                  marginRight: '1rem',
                 }}
               />
             </Link>
           </h4>
         </div>
         {this.state.showContactInfo ? (
-          <div className="card-body">
-            <ul className="list-group">
-              <li className="list-group-item">Email: {email}</li>
-              <li className="list-group-item">Phone: {phone}</li>
+          <div className='card-body'>
+            <ul className='list-group'>
+              <li className='list-group-item'>Email: {email}</li>
+              <li className='list-group-item'>Phone: {phone}</li>
             </ul>
           </div>
         ) : null}
@@ -55,7 +55,4 @@ class Contact extends Component {
   }
 }
 
-export default connect(
-  null,
-  { deleteContact }
-)(Contact);
+export default connect(null, { deleteContact })(Contact);
